d6c87a50b9112b8824879268c8210790e43b4381
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-20 02:15:11 -0600

    added opening game instuctions

--- game.js
+++ game.js
@@ -35,6 +35,10 @@ BasicGame.Game.prototype = {
     this.shotDelay = 100;
 
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.instructions = this.add.text( 400, 500, 'Use Arrow Keys to Move, Press Z to Fire\n' + 'Tapping/clicking does both', {font: '20px monospace', fill: '#fff', align: 'center'});
+    this.instructions.anchor.setTo(0.5, 0.5);
+    this.instExpire = this.time.now + 10000;
   },
 
   update: function () {
@@ -65,6 +69,10 @@ BasicGame.Game.prototype = {
 
     if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
       this.fire();
+    }
+
+    if (this.instructions.exists && this.time.now > this.instExpire) {
+      this.instructions.destroy();
     }
   },
 
