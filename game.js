3e63bda789b670b8d34f04fab3404e8eb5371311
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-20 02:03:17 -0600

    added player firing

--- game.js
+++ game.js
@@ -30,20 +30,19 @@ BasicGame.Game.prototype = {
     this.enemy.play('fly');
     this.enemy.anchor.setTo(0.5, 0.5);
     this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
-
-    this.bullet = this.add.sprite(400, 400, 'bullet');
-    this.bullet.anchor.setTo(0.5, 0.5);
-    this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
-    this.bullet.body.velocity.y = -500;
+    this.bullets = [];
+    this.nextShotAt = 0;
+    this.shotDelay = 100;
 
     this.cursors = this.input.keyboard.createCursorKeys();
   },
+
   update: function () {
     //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
     this.sea.tilePosition.y += 0.2;
-    this.physics.arcade.overlap(
-      this.bullet, this.enemy, this.enemyHit, null, this
-      );
+    for (var i = 0; i < this.bullets.length; i++) {
+      this.physics.arcade.overlap( this.bullets[i], this.enemy, this.enemyHit, null, this);
+    }
 
     this.player.body.velocity.x = 0;
     this.player.body.velocity.y = 0;
@@ -63,6 +62,24 @@ BasicGame.Game.prototype = {
     if (this.input.activePointer.isDown && this.physics.arcade.distanceToPointer(this.player) > 15) {
       this.physics.arcade.moveToPointer(this.player, this.player.speed);
     }
+
+    if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
+      this.fire();
+    }
+  },
+
+  fire: function () {
+    if (this.nextShotAt > this.time.now) {
+      return;
+    }
+    this.nextShotAt = this.time.now + this.shotDelay;
+
+    var bullet = this.add.sprite(this.player.x, this.player.y - 20, 'bullet');
+
+    bullet.anchor.setTo(0.5, 0.5);
+    this.physics.enable(bullet, Phaser.Physics.ARCADE);
+    bullet.body.velocity.y = -500;
+    this.bullets.push(bullet);
   },
 
   enemyHit: function (bullet, enemy) {
