e95f388aaf73cc718aee7a2d511c0e8dcc48e0e0
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-10-19 23:51:11 -0600

    added player sprite and player controls

--- game.js
+++ game.js
@@ -10,11 +10,21 @@ BasicGame.Game.prototype = {
     this.load.image('bullet', 'assets/bullet.png');
     this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
     this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+    this.load.spritesheet('player', 'assets/player.png', 64, 64);
   },
 
   create: function () {
 
     this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
+
+    this.player = this.add.sprite(400, 550, 'player');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('fly', [0, 1, 2], 20, true);
+    this.player.play('fly');
+    this.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.speed = 300;
+    this.player.body.collideWorldBounds = true;
+
     this.enemy = this.add.sprite(400, 200, 'greenEnemy');
     this.enemy.animations.add('fly', [0, 1, 2], 20, true);
     this.enemy.play('fly');
@@ -26,7 +36,7 @@ BasicGame.Game.prototype = {
     this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
     this.bullet.body.velocity.y = -500;
 
-
+    this.cursors = this.input.keyboard.createCursorKeys();
   },
   update: function () {
     //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
@@ -34,6 +44,25 @@ BasicGame.Game.prototype = {
     this.physics.arcade.overlap(
       this.bullet, this.enemy, this.enemyHit, null, this
       );
+
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -this.player.speed;
+    } else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x = this.player.speed;
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.body.velocity.y = -this.player.speed;
+    } else if (this.cursors.down.isDown) {
+      this.player.body.velocity.y = this.player.speed;
+    }
+
+    if (this.input.activePointer.isDown && this.physics.arcade.distanceToPointer(this.player) > 15) {
+      this.physics.arcade.moveToPointer(this.player, this.player.speed);
+    }
   },
 
   enemyHit: function (bullet, enemy) {
